65c239a14034db98fe3b944c615a8465c2afadf3
damienbod <damien_bod@hotmail.com>
2021-04-04 12:41:04 +0200

    Updating ui css and migrations

--- src/NationalDrivingLicense/Migrations/20210404103047_init_fido2.Designer.cs
+++ src/NationalDrivingLicense/Migrations/20210404103047_init_fido2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NationalDrivingLicense.Data;
 
 namespace NationalDrivingLicense.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210404103047_init_fido2")]
+    partial class init_fido2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
