8636a2cb155ac0881420c750b4128ab2d60bcdfa
damienbod <damien_bod@hotmail.com>
2021-04-19 23:35:11 +0200

    updating DB

--- src/NationalDrivingLicense/Migrations/20210419213450_update_mattr_vc.Designer.cs
+++ src/NationalDrivingLicense/Migrations/20210419213450_update_mattr_vc.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NationalDrivingLicense.Data;
 
 namespace NationalDrivingLicense.Migrations
 {
     [DbContext(typeof(NationalDrivingLicenseMattrContext))]
-    partial class NationalDrivingLicenseMattrContextModelSnapshot : ModelSnapshot
+    [Migration("20210419213450_update_mattr_vc")]
+    partial class update_mattr_vc
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/NationalDrivingLicense/Migrations/20210419213450_update_mattr_vc.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NationalDrivingLicense.Migrations
+{
+    public partial class update_mattr_vc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DriverLicenseCredentials",
+                table: "DriverLicenseCredentials");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "DriverLicenseCredentials");
+
+            migrationBuilder.RenameColumn(
+                name: "OfferUrl",
+                table: "DriverLicenseCredentials",
+                newName: "OidcIssuer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "DriverLicenseCredentials",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "DriverLicenseCredentials",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OidcIssuerId",
+                table: "DriverLicenseCredentials",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DriverLicenseCredentials",
+                table: "DriverLicenseCredentials",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DriverLicenseCredentials",
+                table: "DriverLicenseCredentials");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "DriverLicenseCredentials");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "DriverLicenseCredentials");
+
+            migrationBuilder.DropColumn(
+                name: "OidcIssuerId",
+                table: "DriverLicenseCredentials");
+
+            migrationBuilder.RenameColumn(
+                name: "OidcIssuer",
+                table: "DriverLicenseCredentials",
+                newName: "OfferUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "DriverLicenseCredentials",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DriverLicenseCredentials",
+                table: "DriverLicenseCredentials",
+                column: "UserName");
+        }
+    }
+}
