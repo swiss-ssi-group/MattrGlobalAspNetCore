a01a23ebe26398c26ef9aed80c37154fe5e4b8d1
damienbod <damien_bod@hotmail.com>
2021-04-21 21:23:43 +0200

    Adding a name for the input property

--- src/NationalDrivingLicense/Pages/Admin.cshtml.cs
+++ src/NationalDrivingLicense/Pages/Admin.cshtml.cs
@@ -1,5 +1,7 @@
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NationalDrivingLicense.Data;
 
@@ -9,20 +11,36 @@ namespace NationalDrivingLicense.Pages
     {
         private readonly MattrCredentialsService _mattrCredentialsService;
 
-        public string DriverLicenseMessage { get; set; } = "Loading credentials";
+        public bool CreatingDriverLicense { get; set; } = true;
         public string Callback { get; set; }
+
+        [BindProperty]
+        public IssuerCredential IssuerCredential { get; set; }
         public AdminModel(MattrCredentialsService mattrCredentialsService)
         {
             _mattrCredentialsService = mattrCredentialsService;
         }
         public void OnGet()
         {
-
+            IssuerCredential = new IssuerCredential();
         }
 
-        public async Task OnPostAsync()
+        public async Task<IActionResult> OnPostAsync()
         {
-            Callback = await _mattrCredentialsService.CreateCredentialsAndCallback("ndlseven");
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Callback = await _mattrCredentialsService.CreateCredentialsAndCallback(IssuerCredential.CredentialName);
+            CreatingDriverLicense = false;
+            return Page();
         }
+    }
+
+    public class IssuerCredential
+    {
+        [Required]
+        public string CredentialName { get; set; }
     }
 }
