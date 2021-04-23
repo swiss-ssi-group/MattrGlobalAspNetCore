45133b19e43cf83be395f7ee9b324aa289e94a68
damienbod <damien_bod@hotmail.com>
2021-04-23 23:31:40 +0200

    Adding the data to the class

--- src/BoInsurance/Controllers/VerificationController.cs
+++ src/BoInsurance/Controllers/VerificationController.cs
@@ -14,13 +14,32 @@ namespace BoInsurance.Controllers
             _boInsuranceDbService = boInsuranceDbService;
         }
 
+        /// <summary>
+        /// {
+        ///  "presentationType": "QueryByExample",
+        ///  "challengeId": "GW8FGpP6jhFrl37yQZIM6w",
+        ///  "claims": {
+        ///      "id": "did:key:z6MkfxQU7dy8eKxyHpG267FV23agZQu9zmokd8BprepfHALi",
+        ///      "name": "Chris",
+        ///      "firstName": "Shin",
+        ///      "licenseType": "Certificate Name",
+        ///      "dateOfBirth": "some data",
+        ///      "licenseIssuedAt": "dda"
+        ///  },
+        ///  "verified": true,
+        ///  "holder": "did:key:z6MkgmEkNM32vyFeMXcQA7AfQDznu47qHCZpy2AYH2Dtdu1d"
+        /// }
+        /// </summary>
+        /// <param name="body"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("[action]")]
-        public IActionResult DrivingLicenseCallback([FromBody]object body)
+        public IActionResult DrivingLicenseCallback([FromBody] object body)
         {
             // api/Verification/DrivingLicenseCallback
             Console.WriteLine(body);
             // TODO verify using challenge
+            // save to database
             // TODO send event to update UI with the data and 
             return Ok();
         }
