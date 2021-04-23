fa26eb19a4b4c69e20f36e88e42e25e1c4b73549
damienbod <damien_bod@hotmail.com>
2021-04-23 09:40:21 +0200

    Adding persistence for presentation template

---
+++ src/BoInsurance/Data/BoInsuranceVerifyMattrContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BoInsurance.Data
+{
+    public class BoInsuranceVerifyMattrContext : DbContext
+    {
+        public BoInsuranceVerifyMattrContext(DbContextOptions<BoInsuranceVerifyMattrContext> options) : base(options)
+        { }
+
+        public DbSet<DrvingLicensePresentationTemplate> DriverLicenseCredentials { get; set; }
+    }
+}
---
+++ src/BoInsurance/Data/DriverLicenseCredentials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BoInsurance.Data
+{
+    public class DrvingLicensePresentationTemplate
+    {
+        [Key]
+        public int Id { get; set; }
+        public string DidId { get; set; }
+        public Guid TemplateId { get; set; }
+        public string MattrPresentationTemplateReponse { get; set; }
+    }
+}
